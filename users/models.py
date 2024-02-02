4d76a313930a7ae4b219d501de9261950ed2222e
MagnetonBora <MagnetonBora@users.noreply.github.com>
2024-02-02 15:01:29 +0000

    Fix file extension in default avatar.

--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ from PIL import Image
 # Extending User Model Using a One-To-One Link
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(default='default_avatar.png', upload_to='profile_images')
+    avatar = models.ImageField(default='default_avatar.jpg', upload_to='profile_images')
 
     def __str__(self):
         return self.user.username
